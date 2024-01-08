6283ed6d61ab371cc2c5f763e2bf5ed01c55d85a
hawariyaw paulos <hawipaul308@gmail.com>
2024-01-08 12:41:19 -0800

    merge sorted linked lists

---
+++ main.py
@@ -0,0 +1,33 @@
+# this leet code asks to merge sorted(ascending) lined lists
+# for example: 1-2-2 and 1-4-5  out-put  1-1-2-2-4-5
+# here we are going to use a dummy node to do this 
+
+Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+
+        return dummy.next
